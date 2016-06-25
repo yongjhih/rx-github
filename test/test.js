10615b1f3da904668df2b2e78e1b368191ccde11
Andrew Chen <yongjhih@gmail.com>
2016-06-26 00:27:54 +0800

    test: update

--- test/test.js
+++ test/test.js
@@ -3,13 +3,6 @@ var expect = chai.expect;
 
 var fetch = require('../index.js');
 describe('rx-github', function() {
-
-	before(function(done) {
-	});
-
-	after(function(done) {
-	});
-
 	it('should not empty repos', function() {
 	});
 });

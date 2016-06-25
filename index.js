382f3973e08db62459bab4607796ea674a8a604e
Andrew Chen <yongjhih@gmail.com>
2016-06-25 21:48:06 +0800

    As module

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 module.exports = RxGitHub;
-function RxGitHub(url, options) {
-    var fetch = require('node-fetch');
-    var Rx = require('rx');
-    return Rx.Observable.fromPromise(fetch(url, options)).flatMap(function (response) {
+
+var Fetch = require('node-fetch');
+var Rx = require('rx');
+
+function noop() {}
+
+function RxGitHub() {
+}
+
+RxGitHub.prototype.fetch = fetch;
+function fetch(url, options) {
+    return Rx.Observable.fromPromise(Fetch(url, options)).flatMap(function (response) {
         var next = response.headers.get('Link').replace(/<([^<]*)>; rel="next".*/, '$1');
-        return Rx.Observable.concat(Rx.Observable.just(response), next ? RxGitHub(next) : Rx.Observable.empty());
-    })
-    .map(function (response) { return response.json(); })
-    .flatMap(function (json) { return Rx.Observable.from(json); });
+        return Rx.Observable.concat(Rx.Observable.just(response), next ? fetch(next) : Rx.Observable.empty());
+    });
+}
+
+RxGitHub.prototype.repos = repos;
+function repos(user) {
+    return fetch('https://api.github.com/users/' + user + '/repos')
+        .map(function (response) { return response.json(); })
+        .flatMap(function (json) { return Rx.Observable.from(json); });
 }
--- repos.js
+++ repos.js
@@ -1,9 +1,5 @@
-repos('yongjhih').subscribe(function (it) {
+var RxGitHub = require('./index');
+var github = new RxGitHub();
+github.repos('yongjhih').subscribe(function (it) {
     console.log(it);
 });
-
-function repos(user) {
-    var RxGitHub = require('./index');
-    return RxGitHub('https://api.github.com/users/' + user + '/repos')
-        .map(function (json) { return json.name; });
-}
